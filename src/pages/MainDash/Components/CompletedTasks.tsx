6d84109ae11207b8de64a09f5255de58c92a7132
Sarah Fisher <82496944+sgf-HonestAnt@users.noreply.github.com>
2021-12-16 23:06:32 +0000

    edit

--- src/pages/MainDash/Components/CompletedTasks.tsx
+++ src/pages/MainDash/Components/CompletedTasks.tsx
@@ -8,8 +8,8 @@ const CompletedTasks = () => {
   return (
     <div className="m-2 p-1 dashboard__tasks-card__task-box">
       {/* <ICOURGENT /> */}
-      you have completed {total_completed} tasks and accrued {total_xp}
-      <BambooPoints /> so far
+      you've completed {total_completed} tasks and accrued {total_xp}
+      <BambooPoints /> so far!
     </div>
   );
 };
