8cb3eb9568cdca71ce3f8b6432e509797a54d393
Sarah Fisher <82496944+sgf-HonestAnt@users.noreply.github.com>
2022-01-20 16:19:53 +0000

    spacing

--- src/pages/__Components/DashComponents/DashStats.tsx
+++ src/pages/__Components/DashComponents/DashStats.tsx
@@ -16,10 +16,10 @@ export default function DashStats(props: DashStatsProps) {
   // const {} = props;
   const state: reduxStateInt = useAppSelector((state: reduxStateInt) => state);
   const { currentTasks } = state;
-  const { total_xp } = state.currentUser.my_user;
+  const { total_xp, notification } = state.currentUser.my_user;
   const { awaited, in_progress, completed } = currentTasks;
   const allTasks = awaited.concat(in_progress, completed);
-  const isBigScreen = useMediaQuery({ query: "(min-width: 1660px)" });
+  const isGt1660 = useMediaQuery({ query: "(max-width: 1559px)" });
   const [allByStatus, setAllByStatus] = useState<genericTaskInt[]>([]);
   const mapData = async () => {
     const allByStatus = await mapByStatus(currentTasks);
@@ -31,14 +31,18 @@ export default function DashStats(props: DashStatsProps) {
   }, []);
   return (
     <div
-      className={`bamboo-card ${isBigScreen?"m-0":"m-1"} dashboard__dash-stats`}> {/* ${isBigScreen && "my-3"} */}
+      className={`bamboo-card ${
+        isGt1660 && notification.length > 0 ? "m-1" : "m-0"
+      } dashboard__dash-stats`}>
+      {" "}
+      {/* ${isBigScreen && "my-3"} */}
       <Row>
         {(total_xp > 0 || awaited.length > 0 || in_progress.length > 0) && (
           <Col sm={12}>
             <PieChartWithPaddingAngle
               deg360={true}
               data={allByStatus}
-              colors={STATUS_COLORS} 
+              colors={STATUS_COLORS}
               stat='status'
               width={200}
               height={200}
