0b5d3aa0e2b8eeff057b449dc242229f7eb18a1a
Sarah Fisher <82496944+sgf-HonestAnt@users.noreply.github.com>
2022-01-21 10:34:10 +0000

    formik on Login!

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -35,35 +35,10 @@ const schema = yup.object().shape({
 
 export default function RegisterPage({ history }: RouteComponentProps) {
   const [loading, setLoading] = useState(false);
-  // const [validate, setValidate] = useState<validateProps>({
-  //   username: null,
-  //   email: null,
-  //   password: null,
-  // });
-  const [form, setForm] = useState({
-    first_name: "",
-    last_name: "",
-    username: "",
-    email: "",
-    password: "",
-  });
-  // const handleChange = async (e: any) => {
-  //   // e.preventDefault();
-  //   // setValidate({ username: null, email: null, password: null });
-  //   console.log(e.target.id, e.target.value);
-  //   // const { id, value } = e.target;
-  //   // console.log({ [id]: value });
-  //   // setForm({
-  //   //   ...form,
-  //   //   [id]: value,
-  //   // });
-  // };
   const handleClick = () => {
     history.push("/login");
   };
   const handleSubmit = async (e: any) => {
-    // e.preventDefault();
-    // console.log(e);
     setLoading(true);
     try {
       console.log("✔️attempt registration!", e);
@@ -94,7 +69,7 @@ export default function RegisterPage({ history }: RouteComponentProps) {
       console.log(error);
     }
   };
-  console.log("REGISTRATION PAGE", form);
+  console.log("REGISTRATION PAGE");
   return (
     <Container fluid>
       <Row className='registration-form'>
@@ -139,7 +114,6 @@ export default function RegisterPage({ history }: RouteComponentProps) {
                         value={values.first_name}
                         onChange={handleChange}
                         isInvalid={!!errors.first_name}
-                        // isValid={touched.first_name && !errors.first_name}
                       />
                       <Form.Control.Feedback type='invalid'>
                         {errors.first_name}
@@ -161,7 +135,6 @@ export default function RegisterPage({ history }: RouteComponentProps) {
                         value={values.last_name}
                         onChange={handleChange}
                         isInvalid={!!errors.last_name}
-                        // isValid={touched.last_name && !errors.last_name}
                       />
                       <Form.Control.Feedback type='invalid'>
                         {errors.last_name}
@@ -205,7 +178,6 @@ export default function RegisterPage({ history }: RouteComponentProps) {
                         value={values.email}
                         onChange={handleChange}
                         isInvalid={!!errors.email}
-                        // isValid={touched.email && !errors.email}
                       />
                       <Form.Control.Feedback type='invalid'>
                         {errors.email}
@@ -227,7 +199,6 @@ export default function RegisterPage({ history }: RouteComponentProps) {
                         value={values.password}
                         onChange={handleChange}
                         isInvalid={!!errors.password}
-                        // isValid={touched.password && !errors.password}
                       />
                       <Form.Control.Feedback type='invalid'>
                         {errors.password}
@@ -250,95 +221,7 @@ export default function RegisterPage({ history }: RouteComponentProps) {
                 )}
               </Form>
             )}
-          </Formik>
-          {/* <Form onSubmit={handleSubmit}>
-            <Form.Group as={Row} controlId='first_name' className='py-1'>
-              <Form.Label column sm='3'>
-                First name
-              </Form.Label>
-              <Col>
-                <Form.Control
-                  type='text'
-                  minLength={3}
-                  value={form.first_name}
-                  placeholder={"Enter first name"}
-                  onChange={handleChange}
-                />
-              </Col>
-            </Form.Group>
-            <Form.Group as={Row} controlId='last_name' className='py-1'>
-              <Form.Label column sm='3'>
-                Last name
-              </Form.Label>
-              <Col>
-                <Form.Control
-                  type='text'
-                  minLength={3}
-                  value={form.last_name}
-                  placeholder={"Enter last name"}
-                  onChange={handleChange}
-                />
-              </Col>
-            </Form.Group>
-            <Form.Group as={Row} controlId='username' className='py-1'>
-              <Form.Label column sm='3'>
-                Username
-              </Form.Label>
-              <Col>
-                <Form.Control
-                  type='text'
-                  minLength={5}
-                  value={form.username}
-                  placeholder={"Enter username"}
-                  onChange={handleChange}
-                />
-                <Form.Text className='code-red m-0'>
-                  {validate.username}
-                </Form.Text>
-              </Col>
-            </Form.Group>
-            <Form.Group as={Row} controlId='email' className='py-1'>
-              <Form.Label column sm='3'>
-                Email
-              </Form.Label>
-              <Col>
-                <Form.Control
-                  type='email'
-                  value={form.email}
-                  placeholder={"Enter email"}
-                  onChange={handleChange}
-                />
-                <Form.Text className='code-red m-0'>{validate.email}</Form.Text>
-              </Col>
-            </Form.Group>
-            <Form.Group as={Row} controlId='password' className='py-1'>
-              <Form.Label column sm='3'>
-                Password
-              </Form.Label>
-              <Col>
-                <Form.Control
-                  type='password'
-                  minLength={8}
-                  value={form.password}
-                  placeholder={"Enter password"}
-                  onChange={handleChange}
-                />
-              </Col>
-            </Form.Group>
-            {loading ? (
-              <Spinner animation='grow' className='my-4' />
-            ) : (
-              <>
-                <LoginBtn
-                  variant='secondary'
-                  label='Login instead?'
-                  handleClick={handleClick}
-                  className='my-3 mr-3'
-                />
-                <SubmitBtn variant='secondary' className='my-3' />
-              </>
-            )}
-          </Form> */}
+          </Formik>          
         </Col>
       </Row>
     </Container>
